8289ab238ebb6f5ea50e405e8eb49bd369f417f4
puz_ya <puzino.y.a@gmail.com>
2017-01-20 22:13:13 +0200

    Coursera 2017 project with images;

---
+++ 8bitFace/app/src/main/java/com/puzino/a8bitface/MainActivityFragment.java
@@ -0,0 +1,22 @@
+package com.puzino.a8bitface;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image_3buttons, container, false);
+    }
+}
